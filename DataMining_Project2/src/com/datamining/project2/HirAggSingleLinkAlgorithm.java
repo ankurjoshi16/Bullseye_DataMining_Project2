022c1ad3bcf8923284104b6374e431f103980f58
Master <Master@Ankur>
2014-10-31 20:40:40 -0400

    Addition to algo

--- DataMining_Project2/src/com/datamining/project2/HirAggSingleLinkAlgorithm.java
+++ DataMining_Project2/src/com/datamining/project2/HirAggSingleLinkAlgorithm.java
@@ -1,20 +1,55 @@
 package com.datamining.project2;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import com.datamining.project2Utils.ProjectUtils;
 
 public class HirAggSingleLinkAlgorithm {
-	
+
+	private String fileName;
+	private int cutLevel;
 	private List<ProjectCluster> clusters;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String mergeOrder = "";
+
+	public HirAggSingleLinkAlgorithm(String fileName, int cutLevel) {
+
+		clusters = new ArrayList<ProjectCluster>();
+		this.fileName = fileName;
+		this.cutLevel = cutLevel;
 	}
-	
-	private List<ProjectCluster> getInitialClusters(){
-		return null;
+
+	@SuppressWarnings("unused")
+	private List<ProjectCluster> getInitialClusters()
+			throws NumberFormatException, IOException {
+
+		Map<Integer, DataPoint> initialMap = ProjectUtils
+				.readFileToInitialMapNorm(fileName);
+
+		for (Integer key : initialMap.keySet()) {
+			ProjectCluster pc = new ProjectCluster();
+			pc.identifier = key.toString();
+			pc.addPoint(initialMap.get(key));
+			clusters.add(pc);
+		}
+		return clusters;
 	}
-	
-	private void mergeClusters(ProjectCluster c1 , ProjectCluster c2){
-		
+
+	@SuppressWarnings("unused")
+	private void mergeClusters(ProjectCluster c1, ProjectCluster c2) {
+		mergeOrder = mergeOrder + "\n" + c1.identifier + " ----> "
+				+ c2.identifier;
+		List<DataPoint> points = new ArrayList<DataPoint>(
+				c1.getAllClusterPoints());
+		points.addAll(c2.getAllClusterPoints());
+		String identifier = c1.identifier + "|" + c2.identifier;
+		ProjectCluster pc = new ProjectCluster();
+		pc.identifier = identifier;
+		pc.addPoints(points);
+		clusters.remove(c1);
+		clusters.remove(c2);
+		clusters.add(pc);
 	}
 }
--- DataMining_Project2/src/com/datamining/project2/ProjectCluster.java
+++ DataMining_Project2/src/com/datamining/project2/ProjectCluster.java
@@ -1,14 +1,24 @@
 package com.datamining.project2;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.datamining.project2Utils.ProjectUtils;
 
 public class ProjectCluster {
 
 	private List<Double> centriod;
 	private List<DataPoint> clusterPoints = new ArrayList<DataPoint>();
 	public double minDiff;
+	public String identifier;
+	public ProjectCluster nearestC;
+
+	public ProjectCluster() {
+		clusterPoints = new ArrayList<DataPoint>();
+	}
 
 	public ProjectCluster(List<Double> centriod) {
 		this.centriod = centriod;
@@ -26,17 +36,22 @@ public class ProjectCluster {
 	public List<DataPoint> getAllClusterPoints() {
 		return clusterPoints;
 	}
-	
-	public List<Integer> getAllKeys(){
+
+	public List<Integer> getAllKeys() {
 		List<Integer> keys = new ArrayList<Integer>();
-		for(DataPoint dp : getAllClusterPoints()){
+		for (DataPoint dp : getAllClusterPoints()) {
 			keys.add(dp.getIndex());
 		}
 		return keys;
 	}
-	
-	public List<Double> getFinalCentriod() {
 
+	public void addPoints(List<DataPoint> points) {
+		for (DataPoint dp : points) {
+			clusterPoints.add(dp);
+		}
+	}
+
+	public List<Double> getFinalCentriod() {
 		List<Double> centroid = new ArrayList<Double>();
 		int dimensions = getAllClusterPoints().get(0).getCoordinates().size();
 		int size = getAllClusterPoints().size();
@@ -56,9 +71,30 @@ public class ProjectCluster {
 		}
 		return centroid;
 	}
-	
-	public void getMinDiff(List<ProjectCluster> ip){
-		
+
+	public void getMinDiff(List<ProjectCluster> ip) {
+		Map<Double, ProjectCluster> matrix = new LinkedHashMap<Double, ProjectCluster>();
+		for (int i = 0; i < ip.size(); i++) {
+			if (this == ip.get(i)) {
+				continue;
+			} else {
+				double minDistance = Double.MAX_VALUE;
+				for (DataPoint dp : this.getAllClusterPoints()) {
+					for (DataPoint dp2 : ip.get(i).getAllClusterPoints()) {
+						double temp = ProjectUtils.getEuclideanDistance(
+								dp.getCoordinates(), dp2.getCoordinates());
+						if (temp < minDistance) {
+							minDistance = temp;
+						}
+					}
+				}
+				matrix.put(minDistance, ip.get(i));
+			}
+		}
+
+		double totalMin = Collections.min(matrix.keySet());
+		this.minDiff = totalMin;
+		this.nearestC = matrix.get(totalMin);
 	}
 
 }
